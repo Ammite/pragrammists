8d64a4f2f9b989c800f02cf326bbea7d5e1066f3
Ammite <baa21v@gmail.com>
2020-09-23 15:15:52 +0600

    Sudoku backtracking

--- main.py
+++ main.py
@@ -1,42 +1,85 @@
-import variables
-import requests
-from time import sleep
-
-url = "https://api.telegram.org/bot" + variables.token + "/"
-
-
-def get_updates():
-    response = requests.get(url + "getUpdates")
-    return response.json()
-
-
-def get_last_update():
-    results = get_updates()['result']
-    total_updates = len(results) - 1
-    if total_updates < 0:
-        total_updates = 0
-    return results[total_updates]
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
 
 
-def get_chat_id(update):
-    chat_id = update['message']['chat']['id']
-    return chat_id
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
 
 
-def send_message(chat_id, message):
-    parameters = {'chat_id': chat_id, 'text': message}
-    response = requests.post(url + "sendMessage", data=parameters)
-    return response
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j)  # row, col
+
+    return None
 
 
-def main():
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
 
-    last_update = get_last_update()
-    chat_id = get_chat_id(last_update)
-    text = last_update['message']['text']
-    if text == "Првиет":
-        send_message(chat_id, "Пока")
+    for i in range(1,10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
 
 
-if __name__ == "__main__":
-    main()
+def valid(bo, num, pos):
+    # Check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
+
+
+print_board(board)
+solve(board)
+print("___________________")
+print()
+print("___________________")
+print_board(board)
